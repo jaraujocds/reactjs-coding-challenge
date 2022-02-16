0c9ec1af5f1c710b146c37d503cf21055bb0a7d3
Jeremias Araujo <jaraujo@codigodelsur.com>
2022-02-16 09:49:56 -0300

    setup react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/home";
+import Character from "./pages/character";
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/character/:id" element={<Character />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
